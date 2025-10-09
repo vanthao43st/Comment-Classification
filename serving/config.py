293572b2b2de6cd60af2fda67174d57741563003
vanthao43st <nvthao43.hnvn@gmail.com>
2025-10-10 00:19:24 +0700

    serving/config.py

--- serving/config.py
+++ serving/config.py
@@ -1,7 +1,11 @@
 import torch
+import os
 import re
 
-MODEL_PATH = "./models/phobert_sentiment"  # Path to the fine-tuned model directory
+# MODEL_PATH = "./models/phobert_sentiment"  # Path to the fine-tuned model directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # thư mục 'serving'
+MODEL_PATH = os.path.join(BASE_DIR, "models", "phobert_sentiment")
+
 URL_REGEX     = re.compile(r'(https?://[^\s]+|www\.[^\s]+)')
 EMAIL_REGEX   = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
 MENTION_REGEX = re.compile(r'@[A-Za-z0-9_]+')
